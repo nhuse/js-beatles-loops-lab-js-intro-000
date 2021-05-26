8b607c2e97ffc5280e2fbd3bb7d38b211abeeb77
Nathan Huse <nathan.huse@yahoo.com>
2021-05-26 17:25:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray) {
+  const array = [];
+  for (var i = 0; i < array.length-1; i++) {
+    let newString = `${musiciansArray[i]} plays ${instrumentsArray[i]}`;
+    array.push(newString);
+  }
+  return array;
+}
