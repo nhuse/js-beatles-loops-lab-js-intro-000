d567fb2ab93cf03d782ca8dcce6f0807614d2c72
Nathan Huse <nathan.huse@yahoo.com>
2021-05-26 17:45:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray) {
+  const array = [];
+  for (var i = 0; i < musiciansArray.length; i++) {
+    let newString = `${musiciansArray[i]} plays ${instrumentsArray[i]}`;
+    array.push(newString);
+  }
+  return array;
+}
+
+function johnLennonFacts(factsArray) {
+  const newArr = [];
+  const exclamationPoint = "!!!";
+  for (var i = 0; i < factsArray.length; i++) {
+    let newString = factsArray[i].concat(exclamationPoint);
+    newArr.push(newString);
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(num) {
+  const newArr = [];
+  do {
+    num = num+1;
+    newArr.push('I love the Beatles!');
+  } while(num<15);
+
+  return newArr;
+}
